2c06b0682e53ef1b8e1cbb232554c491edd8d0a2
Adam Toro <toro04@gmail.com>
2013-10-06 18:47:11 -0400

    Put delivery_date after shipping_methods option

--- app/overrides/delivery_date.rb
+++ app/overrides/delivery_date.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/checkout/_delivery",
                      :name => "add_delivery_date_to_delivery",
-                     :insert_after => "[data-hook='shipping_method_inner']",
+                     :insert_after => "[data-hook='shipping_method_inner'] ul",
                      :partial => "spree/checkout/delivery_date",
                      :disabled => false)
